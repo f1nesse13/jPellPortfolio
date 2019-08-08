3270dacb50c32847dfa304e47784995a02f99cf6
Joe Pell <jpell3130@gmail.com>
2019-08-07 22:33:47 -0400

    migration and addition of user model for auth

---
+++ db/migrate/20190806204406_create_users_table.rb
@@ -0,0 +1,10 @@
+class CreateUsersTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users_tables do |t|
+      t.string :username, null: false
+      t.string :email
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+    end
+  end
+end
