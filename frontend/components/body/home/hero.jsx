7d618c056968a654e28fde1386823f9feb375338
Joe Pell <jpell3130@gmail.com>
2019-08-07 22:42:48 -0400

    started homepage with hero

---
+++ frontend/components/body/home/hero.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Hero extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Hi I'm Joe a Software Developer</h1>
+        <p>I am a full-stack web developer based out of Philadelphia, PA.</p>
+        <p>
+          I love all things code and love to create web applications and sites.
+          I work with many different technologies but mainly with Rails with
+          React. To hear more about me please have a look at my about page
+          below. If you'd like to see some of my projects, contact me or hear a
+          little about my everyday grind please follow the links below. Thanks
+          for visiting!
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Hero;
---
+++ frontend/components/body/home/home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Hero from "./hero";
+
+const Home = props => {
+  return <Hero />;
+};
+
+export default Home;
