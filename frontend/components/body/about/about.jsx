e87fbec071af4fb51467a896ce9b9ac1fa424f6e
Joe Pell <jpell3130@gmail.com>
2019-08-17 17:36:58 -0400

    added content to about page

--- frontend/components/body/about/about.jsx
+++ frontend/components/body/about/about.jsx
@@ -6,13 +6,12 @@ class About extends Component {
       <section className="about">
         <h1>About Me</h1>
         <p>
-          I am a 32 year old full-stack developer working with mostly Rails and
+          I am a 32 year old fullstack developer working with mostly Rails and
           React. Although they are the main technologies I use for web
-          applications I use some Ruby for scripting and have some prior
-          experience with nodejs for backend programming. Some other tools I am
-          familiar with are SQL, NoSQL databases such as postgres, SQLite and
-          mongoDB. I also have experience with consuming as well as creating
-          RESTful APIs.
+          applications I use some Ruby for scripting and have some knowledge of
+          nodejs for backend programming. Some other tools I am familiar with
+          are SQL and NoSQL databases such as postgres, SQLite and mongoDB as
+          well as experience consuming and creating RESTful APIs.
         </p>
         <p>
           I've been writing code on and off since high school and have always
@@ -24,7 +23,25 @@ class About extends Component {
           seriously, consuming information from every source I could find.
           Development really draws me in and allows me to be creative, at times
           it is hard for me to tear myself away. I am a a great problem solver
-          and love to be challenged.
+          and love to be challenged. Development is a perfect fit for my
+          skillset.
+        </p>
+        <p>
+          As I become more entrenched in development I see myself leaning more
+          and more towards the frontend - specifically with React. Although I
+          really enjoy backend programming I see myself gravitating towards the
+          frontend. So if I had to describe my speciality I would say I'm a
+          fullstack developer specializing in React. Im my schooling we focused
+          on full stack development with Ruby and Javascript. We started with
+          Ruby fundamentals and moved onto Rails rather quickly. My skills in
+          writing Ruby code became more and more refined. A lot of work I did
+          with Ruby naturally led me to questions about computer science topics
+          that I was then able to apply to Javascript and all other technologies
+          I used afterwards. When we started Javascript I was familiar with it
+          but had forgotten a lot of what I knew before. As with Ruby I quickly
+          became accoustomed to writing Javascript and later, React all over
+          again. Its been a fun journey to this point and I look forward to
+          continue learning about new technology as I continue my career.
         </p>
       </section>
     );
