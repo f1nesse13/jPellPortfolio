2f75cd7bf102545fb9fbc3852f5a97af06e43dda
Joe Pell <jpell3130@gmail.com>
2019-08-20 12:24:08 -0400

    added landing page - will direct users to our links

---
+++ frontend/components/body/landing/landing.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <header className="landing_header">
+      <div className="landing_content">
+        <h1 className="landing_title">
+          Welcome to Finesse <span>Designs</span>
+        </h1>
+        <p className="landing_text">
+          Please use the links below to navigate - thanks for visiting!
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Landing;
