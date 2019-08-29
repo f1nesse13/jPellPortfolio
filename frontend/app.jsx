45e7c1519043a4fdd337056c27bb3d7a1f6bde6f
Joe Pell <jpell3130@gmail.com>
2019-08-29 12:32:30 -0400

    added arrows to page for switching views // added tmp to gitignore

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -5,11 +5,12 @@ import Home from "./components/body/home/home";
 import About from "./components/body/about/about";
 import IntroModal from "./components/modal/intro_modal";
 import Landing from "./components/body/landing/landing";
-
+import Arrow from './components/arrow'
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { active: "" };
+    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
@@ -25,19 +26,18 @@ class App extends Component {
     });
     window.sessionStorage.intro = false;
   }
+
   render() {
     return (
       <div className="app_window">
-        {this.state.active ? (
-          <IntroModal handleClick={this.handleClick.bind(this)} />
-        ) : (
-          ""
-        )}
+        {this.state.active ? <IntroModal handleClick={this.handleClick} /> : ''}
+        <Arrow arrowDirection="left" />
         <Route path="/" component={NavBar} />
         <Route exact path="/" component={Landing} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/about" component={About} />
-        {this.state.active ? <div className="modal_bg"></div> : ""}
+        <Arrow arrowDirection='right' />
+        {this.state.active ? <div className="modal_bg" /> : ""}
       </div>
     );
   }
