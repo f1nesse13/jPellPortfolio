92f8f22c4ae1f75ac6b127ffc3f3e48912987eec
Joe Pell <jpell3130@gmail.com>
2019-09-02 20:15:56 -0400

    placeholder state / placeholder iteration for adding projects

--- frontend/components/body/projects/projects.jsx
+++ frontend/components/body/projects/projects.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 
 class Projects extends Component {
+  state = {
+    project_list: {
+      title: 'Barking Spyder Pro Shop',
+      image: 'url here',
+      details: 'yada yada',
+    },
+  };
   render() {
+
+    this.state.project_list.map(project => <Project name={}/>)
     return <div>Projects</div>;
   }
 }
